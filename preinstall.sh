368e91eba9c4f9bfe4b671dc5483446272aa9d84
Christopher Weckert <info@chris-weckert.de>
2021-09-22 13:30:12 +0200

    added krew and kubectx and kubens

--- preinstall.sh
+++ preinstall.sh
@@ -27,6 +27,21 @@ if [ ! -f "$HOME/.local/bin/kubectl" ]; then
     mv ./kubectl ~/.local/bin/
 fi
 
+if [ ! -d "~/.krew" ]; then
+(
+    set -x; cd "$(mktemp -d)" &&
+        OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+        ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+        curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.tar.gz" &&
+        tar zxvf krew.tar.gz &&
+        KREW=./krew-"${OS}_${ARCH}" &&
+        "$KREW" install krew
+)
+fi
+
+kubectl krew install ctx
+kubectl krew install ns
+
 # Install Fira Code Font
 font_dir="$HOME/.local/share/fonts"
 if [ ! -d "$font_dir/FiraCode" ]; then
